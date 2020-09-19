3c38be176a8322c541bfa6960a0b99436a7fe1de
mida-hub <rusuden0106@gmail.com>
2020-09-20 01:06:02 +0900

    footer

--- django_vue/frontend/src/components/Footer.vue
+++ django_vue/frontend/src/components/Footer.vue
@@ -1,16 +1,12 @@
 <template>
-  <v-card height="50">
-    <v-footer
-      absolute
-      class="font-weight-medium"
-    >
-      <v-col>
-        <div class="blog-footer">
-          <small>&copy; とまとエンジニア日記 {{ new Date().getFullYear() }}</small>
-        </div>
-      </v-col>
-    </v-footer>
-  </v-card>
+  <v-footer
+    absolute
+    class="font-weight-medium"
+  >
+    <v-col style="text-align: center;">
+      <small>&copy; とまとエンジニア日記 {{ new Date().getFullYear() }}</small>
+    </v-col>
+  </v-footer>
 </template>
 
 <script>
